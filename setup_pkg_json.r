8cfead741a27881133bd5fcaad59db259b2a53bd
SpatLyu <lyu.geosocial@gmail.com>
2024-08-21 00:52:34 +0800

    add geosimilarity

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,9 +1,12 @@
 setwd('c:/Users/dell/Downloads/pkgdev/ausgis_r-uni/')
 
 df = data.frame(
-  package = c("gdverse", "geocomplexity"),
+  package = c("gdverse", 
+              "geocomplexity",
+              "geosimilarity"),
   url = c("https://github.com/ausgis/gdverse", 
-          "https://github.com/ausgis/geocomplexity"),
-  subdir = rep(NA,2)
+          "https://github.com/ausgis/geocomplexity",
+          "https://github.com/ausgis/geosimilarity"),
+  subdir = rep(NA,3)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
