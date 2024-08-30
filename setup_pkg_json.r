010f519e9413514adef2bd4b784477e5b1f169d8
SpatLyu <lyu.geosocial@gmail.com>
2024-08-30 15:09:14 +0800

    add SecDim package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -3,10 +3,12 @@ setwd('c:/Users/dell/Downloads/pkgdev/ausgis_r-uni/')
 df = data.frame(
   package = c("gdverse", 
               "geocomplexity",
-              "geosimilarity"),
+              "geosimilarity",
+              "SecDim"),
   url = c("https://github.com/ausgis/gdverse", 
           "https://github.com/ausgis/geocomplexity",
-          "https://github.com/ausgis/geosimilarity"),
-  subdir = rep(NA,3)
+          "https://github.com/ausgis/geosimilarity",
+          "https://github.com/ausgis/SecDim"),
+  subdir = rep(NA,4)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
