5a45ee8b89b5513b995ec8911a4272af8a9f89bc
SpatLyu <lyu.geosocial@gmail.com>
2024-10-16 15:29:46 +0800

    rm gdverse

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,14 +1,12 @@
 setwd('c:/Users/dell/Downloads/pkgdev/ausgis_r-uni/')
 
 df = data.frame(
-  package = c("gdverse", 
-              "geocomplexity",
+  package = c("geocomplexity",
               "geosimilarity",
               "SecDim"),
-  url = c("https://github.com/ausgis/gdverse", 
-          "https://github.com/ausgis/geocomplexity",
+  url = c("https://github.com/ausgis/geocomplexity",
           "https://github.com/ausgis/geosimilarity",
           "https://github.com/ausgis/SecDim"),
-  subdir = rep(NA,4)
+  subdir = rep(NA,3)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
