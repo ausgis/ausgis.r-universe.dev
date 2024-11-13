92dad65b267f413fcd7aaf13702a922ed711d089
SpatLyu <lyu.geosocial@gmail.com>
2024-11-14 00:16:24 +0800

    add cisp pkg

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -4,11 +4,13 @@ df = data.frame(
   package = c("geocomplexity",
               "geosimilarity",
               "SecDim",
-              "GD"),
+              "GD",
+              "cisp"),
   url = c("https://github.com/ausgis/geocomplexity",
           "https://github.com/ausgis/geosimilarity",
           "https://github.com/ausgis/SecDim",
-          "https://github.com/ausgis/GD"),
-  subdir = rep(NA,4)
+          "https://github.com/ausgis/GD",
+          "https://github.com/ausgis/cisp"),
+  subdir = rep(NA,5)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
