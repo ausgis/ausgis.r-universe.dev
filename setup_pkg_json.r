528f77a52d0b2fb3e2fb944c361ce44f1b54de2f
SpatLyu <lyu.geosocial@gmail.com>
2025-01-19 19:22:31 +0800

    add `localsp` pkg

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -4,11 +4,13 @@ df = data.frame(
   package = c("geocomplexity",
               "geosimilarity",
               "SecDim",
-              "GD"),
+              "GD",
+              "localsp"),
   url = c("https://github.com/ausgis/geocomplexity",
           "https://github.com/ausgis/geosimilarity",
           "https://github.com/ausgis/SecDim",
-          "https://github.com/ausgis/GD"),
-  subdir = rep(NA,4)
+          "https://github.com/ausgis/GD",
+          "https://github.com/ausgis/localsp"),
+  subdir = rep(NA,5)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
